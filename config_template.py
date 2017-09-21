b1965946ee250fe0be9181b357e75318eb335dfa
connor-philip <connorphilip12@hotmail.com>
2017-09-22 00:14:17 +0100

    config template

---
+++ config_template.py
@@ -0,0 +1,15 @@
+emailConfig = {
+    "senderAddress": "email to send from",
+    "senderAddressPassword": "password of email to send from",
+    "mailServer": "mail server to connect to for the sender email",
+    "targetAddress": "email to notify to"
+}
+NotificationConfigs = [
+    {
+    "name": "name of the notification. User defined",
+    "feed": "feed url",
+    "search": "search string or index int",
+    "storeList": ["keys", "to", "store"],
+    "mailTemplate": "notification_template.txt"
+    }
+]
