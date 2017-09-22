994c59908c1757a13238d4b514daadf6be1011c0
connor-philip <connorphilip12@hotmail.com>
2017-09-22 23:15:03 +0100

    tidying some files into folder

--- Vagrantfile
+++ Vagrantfile
@@ -3,5 +3,5 @@ Vagrant.configure("2") do |config|
   config.vm.box_version = "2.3.1"
   config.vm.synced_folder ".", "/vagrant", disabled: true
   config.vm.synced_folder ".", "/home/vagrant/hermercury"
-  config.vm.provision :shell, name: "setup", path: "setup.sh"
+  config.vm.provision :shell, name: "setup", path: "environment/setup.sh"
 end
--- environment/setup.sh
+++ environment/setup.sh
@@ -3,10 +3,15 @@ echo "export PYTHONPATH=/home/vagrant/hermercury" >> /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 
 
-#Install python, pip, and python modules
+# Install python, pip, and python modules
 apt-get update
 apt-get -y install python
 apt-get -y install python-pip
 pip install --upgrade pip
 
 pip install feedparser
+
+
+# Setup the cron
+chmod +x /home/vagrant/hermercury/main.py
+crontab /home/vagrant/hermercury/environment/hermercury_crontab
