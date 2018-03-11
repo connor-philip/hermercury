f5f1d14d48b4bcb7d5653700a245a2a0118db936
connor-philip <connorphilip12@hotmail.com>
2018-03-11 21:23:01 +0000

    Adding a cli which allows when to choose frequency and to run in the background

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from config import NotificationConfigs
-import rss
+from process_control import ProcessControl
+import schedule
+import argparse
 import notify
+import time
+import rss
 import os
 
 PROJECTDIR = os.path.dirname(os.path.abspath(__file__))
+PIDFILE = os.path.join(PROJECTDIR, "hermercury.pid")
+parser = argparse.ArgumentParser(prog="command")
+subparsers = parser.add_subparsers(help='sub-command help')
 
 
 class Notification:
@@ -32,9 +39,43 @@ class Notification:
         notify.send_email(Email)
 
 
-# Main loop
-for NotificationConfig in NotificationConfigs:
-    Instance = Notification(NotificationConfig)
-    Instance.search_for_notification()
-    if Instance.Entry and Instance.NotificationPending:
-        Instance.send_notification()
+def main():
+    for NotificationConfig in NotificationConfigs:
+        Instance = Notification(NotificationConfig)
+        Instance.search_for_notification()
+        if Instance.Entry and Instance.NotificationPending:
+            Instance.send_notification()
+
+
+def start_scheduler(args):
+    frequency = args.frequency
+
+    schedule.every(frequency).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def start_background_process(args):
+    frequency = str(args.frequency)
+    ProcessControl(PIDFILE).create_process(["python", os.path.abspath(__file__), "start", "-f", frequency, "--foreground"])
+
+
+def stop_background_process(args):
+    ProcessControl(PIDFILE).stop_process()
+
+
+startParser = subparsers.add_parser("start", help="Starts Hermercury")
+startParser.add_argument("-f", "--frequency", type=int, default=15, help="Sets the frequency at which Hermercury should run. In minutes. Default set to 15")
+startParser.add_argument("--foreground", action="store_true", help="Starts Hermercury in the foreground. Does not change the currently running process")
+startParser.set_defaults(commandFunction=start_background_process)
+
+stopParser = subparsers.add_parser("stop", help="Stops Hermercury")
+stopParser.set_defaults(commandFunction=stop_background_process, foreground=False)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.foreground:
+        start_scheduler(args)
+    else:
+        args.commandFunction(args)
